158b1e23391994bb65f368014e9690f5c637fa30
Tomasz Gromadko <tgromadko@gmail.com>
2022-03-30 13:56:01 +0200

    add Rating js

--- force-app/main/default/lwc/addRating/addRating.js
+++ force-app/main/default/lwc/addRating/addRating.js
@@ -1,3 +1,45 @@
 import { LightningElement } from 'lwc';
+import { createRecord } from 'lightning/uiRecordApi';
 
-export default class AddRating extends LightningElement {}
+import REVIEW_OBJECT from "@salesforce/schema/Review";
+
+export default class AddRating extends LightningElement {
+
+    reviewRecord = {};
+
+    //flag
+    isLoading = false;
+
+    hanldeChange(event) {
+        this.reviewRecord[event.target.name] = event.target.name;
+    }
+    createReview(){
+        this.isLoading = true;
+        const fields = this.reviewRecord;
+        const recordInput = {apiName: REVIEW_OBJECT.objectApiName, fields};
+
+createRecord(recordInput).then((Review)=>{
+    this.reviewId = Review.id;
+    this.dispatchEvent(
+        new ShowToastEvent({
+            title: "Success",
+            message: "Review Created successfully!",
+            variant: "sucess"
+        })
+    );
+    this.reviewRecord = {};
+    })
+    .catch((error) => {
+        this.dispatchEvent(
+            new ShowToastEvent({
+                title: "Error creating record",
+                message: reduceErrors(error).join(", "),
+                variant: "error"
+            })
+        );
+    })
+    .finally(() => {
+        this.isLoading = false;
+    });
+}
+}
